00c312c160dc15bf03f2b97410e198520cbb1070
usoft-projects <usoft.projects@gmail.com>
2022-09-21 15:03:53 +0300

    data export

--- js/demo/chart-bar-demo.js
+++ js/demo/chart-bar-demo.js
@@ -37,8 +37,94 @@ function update_chart(passed, failed,leng){
       labels: ["Passed", "Failed"],
       datasets: [{
         label: "Number of ",
-        backgroundColor: "#4e73df",
-        hoverBackgroundColor: "#2e59d9",
+        backgroundColor: ["#4e73df","#e74a3b"],
+        hoverBackgroundColor: ["#4e73df","#e74a3b"],
+        borderColor: "#4e73df",
+        data: [passed, failed],
+      }],
+    },
+    options: {
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 0
+        }
+      },
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'status'
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          ticks: {
+            maxTicksLimit: 6
+          },
+          maxBarThickness: 25,
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: leng,
+            maxTicksLimit: 5,
+            padding: 10,
+            // Include a dollar sign in the ticks
+            callback: function(value, index, values) {
+              return '' + number_format(value);
+            }
+          },
+          gridLines: {
+            color: "rgb(234, 236, 244)",
+            zeroLineColor: "rgb(234, 236, 244)",
+            drawBorder: false,
+            borderDash: [2],
+            zeroLineBorderDash: [2]
+          }
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        titleMarginBottom: 10,
+        titleFontColor: '#6e707e',
+        titleFontSize: 14,
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        caretPadding: 10,
+        callbacks: {
+          label: function(tooltipItem, chart) {
+            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+            return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+          }
+        }
+      },
+    }
+  });
+}
+
+
+function update_chart_2(passed, failed,leng){
+  // Bar Chart Example
+  var ctx = document.getElementById("myBarChart_2");
+  var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Passed", "Failed"],
+      datasets: [{
+        label: "Number of ",
+        backgroundColor: ["#4e73df","#e74a3b"],
+        hoverBackgroundColor: ["#4e73df","#e74a3b"],
         borderColor: "#4e73df",
         data: [passed, failed],
       }],
--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ allText = allText.split("\n")
 var PASSED = []
 var FAILED = []
 var branch = []
-var all_clear_data =[]
+var all_clear_data = []
 
 for(var i = 0; i<allText.length;i++){
   if(allText[i].includes("_wo_license)")){
@@ -55,15 +55,17 @@ document.getElementById("last_date_test").innerHTML = mySubString
 
 
 var branch_list = document.getElementById("branch_list")
-
+var branch_list_2 = document.getElementById("branch_list_2")
 for(var i = 0;i<branch_last.length;i++){
   branch_list.innerHTML += '<a class="dropdown-item" href="#" onclick="Details_Branch(this)" id ="'+branch_last[i]+'">'+branch_last[i]+'</a>'
+  branch_list_2.innerHTML += '<a class="dropdown-item" href="#" onclick="Details_Branch_2(this)" id ="'+branch_last[i]+'">'+branch_last[i]+'</a>'
 }
 
-var data_chart_failed = ""
-var data_chart_passed = ""
 
+var amount_1 = ""
 function Details_Branch(d){
+    var data_chart_failed = ""
+    var data_chart_passed = ""
     document.getElementById("bar_title_1").innerHTML = d.id
     var tmp_fail = []
     var tmp_pass = []
@@ -81,4 +83,55 @@ function Details_Branch(d){
     console.log(data_chart_passed)
     console.log(data_chart_failed)
     update_chart(data_chart_passed,data_chart_failed,(data_chart_passed+data_chart_failed))
+    document.getElementById("information_1_pass").innerHTML = "Passed: " +data_chart_passed
+    document.getElementById("information_1_fail").innerHTML = "Failed: " +data_chart_failed
+    if (data_chart_failed > data_chart_passed){
+        document.getElementById("information_1_status").innerHTML = '<i class="fas fa-sad-tear" style="color: #e74a3b;"></i>'
+    }else{
+        document.getElementById("information_1_status").innerHTML = '<i class="fas fa-smile" style="color: #36b9cc;"></i>'
+    }
+    amount_1 = (100 * data_chart_passed) / (data_chart_passed + data_chart_failed)
+    console.log(amount_1)
+}
+
+var amount_2 = ""
+
+function Details_Branch_2(d){
+    var data_chart_failed = ""
+    var data_chart_passed = ""
+    document.getElementById("bar_title_2").innerHTML = d.id
+    var tmp_fail = []
+    var tmp_pass = []
+    for (var i = 0;i<all_clear_data.length;i++){
+        // console.log(all_clear_data[i])
+        if(all_clear_data[i].includes(d.id) && all_clear_data[i].includes("PASSED")){
+            tmp_pass.push(all_clear_data[i])
+        }
+        else if(all_clear_data[i].includes(d.id) && all_clear_data[i].includes("FAILED")){
+            tmp_fail.push(all_clear_data[i])
+        }
+    }
+    data_chart_failed = tmp_fail.length 
+    data_chart_passed = tmp_pass.length
+    console.log(data_chart_passed)
+    console.log(data_chart_failed)
+    update_chart_2(data_chart_passed,data_chart_failed,(data_chart_passed+data_chart_failed))
+    document.getElementById("information_2_pass").innerHTML = "Passed: " +data_chart_passed
+    document.getElementById("information_2_fail").innerHTML = "Failed: " +data_chart_failed
+    if (data_chart_failed > data_chart_passed){
+        document.getElementById("information_2_status").innerHTML = '<i class="fas fa-sad-tear" style="color: #e74a3b;"></i>'
+    }else{
+        document.getElementById("information_2_status").innerHTML = '<i class="fas fa-smile" style="color: #36b9cc;"></i>'
+    }
+    amount_2 = (100 * data_chart_passed) / (data_chart_passed + data_chart_failed)
+    console.log(amount_2)
+    last_status(amount_1, amount_2)
+}
+function last_status(amount_1, amount_2){
+    var status = document.getElementById("last_status")
+    if (amount_1 > amount_2){
+        status.innerHTML = '<a class="btn btn-info btn-sm" href="#!" ><i class="fas fa-smile"></i>&emsp;Good</a>'
+    }else{
+        status.innerHTML = '<a class="btn btn-danger btn-sm" href="#!" ><i class="fas fa-sad-tear"></i>&emsp;Bad</a>'
+    }
 }
